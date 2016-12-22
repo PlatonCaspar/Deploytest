95d73dccc3038279b63d94abde8ff9676c4614c4
Stefan Steinmüller <stefan.steinmueller@siemens.com>
2016-12-22 20:13:02 +0100

    Logo works, still trying to get a Searchbar to nav. now displaying an error when adding an existing board now adding the html file for the board history HTML-File for the history is called. and called correctly. but still empty and i dont have aclue right now //This is fine now. Now I have to find out how to edit the Text in the database via a WYSIWYG Editor for the User AAAAAAAND I can finally display Items on the right side of the navigationbar. I can register and delete and Save users with hashed passwords! Trying to implement or get this wysiwyg Editor to do what I want!!

--- addPlatineForm.py
+++ addPlatineForm.py
@@ -8,7 +8,6 @@ class BoardForm(Form):
     code = StringField('Code', [validators.data_required])
     name = StringField('Project Name', [validators.DataRequired])
     ver = StringField('Version', [validators.data_required])
-    history = StringField('History', validators=[validators.required],
-                          body=WysiwygField('editor', validators=[validators.required]))
+    history = WysiwygField('editor')
     submit = SubmitField('Create')
     hidden_tag = HiddenField('Blubb')
