d5ccad7ef84561c031e8ab4e9902c8fee10a9bb9
Stefan <steinmueller.stefan@siemens.com>
2017-06-07 15:53:44 +0200

    Added Upgrade_Support from browser

--- upgrade.py
+++ upgrade.py
@@ -1,9 +1,23 @@
-from data_Structure import app, db 
-from flask_script import Manager 
-from flask_migrate import Migrate, MigrateCommand 
-migrate = Migrate(app, db) 
-manager = Manager(app) 
-manager.add_command('db', MigrateCommand) 
-if __name__ == '__main__':
-    manager.run()
+from data_Structure import app, db
+# from flask_script import Manager
 
+from flask import render_template, current_app
+from flask_migrate import Migrate, init, migrate, stamp, upgrade
+import os
+
+MIGRATIONS_FOLDER = "migrations/migrations/"
+
+migration = Migrate(app, db, render_as_batch=True)
+
+
+# manager = Manager(app)
+# manager.add_command('db', MigrateCommand)
+def migrate_database():
+    if not os.path.exists(MIGRATIONS_FOLDER):
+        print(init(directory=MIGRATIONS_FOLDER))
+    print("Stamp" + str(stamp(directory=MIGRATIONS_FOLDER, revision='head')))
+    print(migrate(directory=MIGRATIONS_FOLDER))
+    print(upgrade(directory=MIGRATIONS_FOLDER))
+
+# if __name__ == '__main__':
+#    manager.run()
