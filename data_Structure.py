3a43cb3b503ce41489bd9dc3490ad61843e5176f
Stefan <steinmueller.stefan@siemens.com>
2017-11-06 15:04:13 +0100

    same as before

--- data_Structure.py
+++ data_Structure.py
@@ -18,21 +18,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///static/Database/data.sql'
 db = flask_sqlalchemy.SQLAlchemy(app)
 
 
-class JSONEncodedDict(TypeDecorator):
-    "Represents an immutable structure as a json-encoded string."
-    
-    impl = VARCHAR
-
-    def process_bind_param(self, value, dialect):
-        if value is not None:
-            value = json.dumps(value)
-        return value
-
-    def process_result_value(self, value, dialect):
-        if value is not None:
-            value = json.loads(value)
-        return value
-
 
 class Board(db.Model):
     code = db.Column(db.String(500), primary_key=True)
