6c11c08d77bdb399de9c22c3c8dbf91908f893e1
Stefan <steinmueller.stefan@siemens.com>
2018-04-11 21:20:01 +0200

    adding part type works at its roots

--- static/customs.js
+++ static/customs.js
@@ -31,34 +31,39 @@ function showSlides(n) {
 //end document slider
 
 // flexible inputs on PartType
-// function add_input(container){
-//         console.log("add_input was called")
-//         var start = 0;
-//         var inputs;
-//         inputs = $(container).find('input');
-//         for (var input in inputs){
-//             var name;
-//             name = input.name;
-//             name = name.split(':')[1];
-//             nr = parseInt(name);
-//             if (nr>start){
-//                 start = nr;
-//             }
-//         }
-//         start += 1;
-//         var _str = "\".input-group\""
-//         container.append(
-//                     '<div class="input-group" id="group:'+toString(start)+'">'+
-//                         '<input class="form-control" placeholder="Please enter a property..."'+
-//                         'id="input:'+toString(start)+'" name="input:'+toString(start)+'">'+
-//                         '<div class="input-group-btn">'+
-//                             '<button class="btn btn-default" type="button" oncklick="$(this).parent('+_str+').remove()">'+
-//                                 '<i class="glyphicon glyphicon-remove"></i>'+
-//                             '</button>'+
-//                         '</div>'+
-//                     '</div>'
-//         )
-//     }
+function add_input(container){
+        console.log("add_input was called")
+        var start = 0;
+        var inputs;
+        inputs = $(container).find('input');
+        console.log(inputs)
+        var input;
+        for (input = 0; input<inputs.length; input++){
+            if (typeof inputs[input] === "undefined"){continue;}
+            console.log(inputs[input]);
+            var name = $(inputs[input]).attr("name");
+            console.log(name);
+            name = name.split(':')[1];
+            nr = parseInt(name);
+            console.log("Parse Int: "+nr)
+            if (nr>start){
+                start = nr;
+            }
+        }
+        start = start+1;
+        var _str = "\".input-group\""
+        container.append(
+                    '<div class="input-group" id="group:'+start+'">'+
+                        '<input class="form-control" placeholder="Please enter a property..."'+
+                        'id="input:'+start+'" name="input:'+start+'">'+
+                        '<div class="input-group-btn">'+
+                            '<button class="btn btn-default" type="button" oncklick="$(this).parent().find('+_str+').remove()">'+
+                                '<i class="glyphicon glyphicon-remove"></i>'+
+                            '</button>'+
+                        '</div>'+
+                    '</div>'
+        )
+    }
 
 function remove_input(div){
   div.remove()
